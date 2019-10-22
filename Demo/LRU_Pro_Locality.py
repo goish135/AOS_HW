f86865ea88a32cd2eb0346f8c8f4165ea26d3b93
goish135 <goish2468@gmail.com>
2019-10-22 13:28:46 +0800

    上傳網大的code

---
+++ Demo/LRU_Pro_Locality.py
@@ -0,0 +1,172 @@
+# LRU Pro
+# example rs : 7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1
+# frame number : 3
+# LRUPro > page fault : 11
+# Optimal > page fault : 9
+# Fifo > page fault : 13 
+
+"""
+priority 1 : 使用間斷次數
+priority 2 : FIFO
+"""
+
+"""
+rs = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1]
+fn = 3
+"""
+"""
+import csv
+with open('LRU_pro_rrs.csv','a',newline='\n') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["numberof_Frame","Page Fault ","Interrupt","disk write"])
+
+import random
+len_rs = 100000
+# randint(1,500) 1~500
+rs = []
+dw_set = [] # disk write set
+for i in range(len_rs):
+    rs.append(random.randint(1,500))
+    dw_set.append(random.randint(0,1))
+"""
+import csv
+with open('LRU_pro_lrs.csv','a',newline='\n') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["numberof_Frame","Page Fault ","Interrupt","disk write"])
+
+import random
+len_rs = 100000
+# randint(1,500) 1~500
+rs = []
+dw_set = [] # disk write set
+
+"""
+for i in range(len_rs):
+    random_rs.append(random.randint(1,500))
+    dw_set.append(random.randint(0,1))
+"""
+ 
+while len(rs) < len_rs: 
+     
+    begin = random.randint(1,500)
+    maxApend = 500 - begin 
+    if maxApend >= 2:
+        add = random.randint(1,maxApend) # 還可放幾個的扣打
+        # to be continue
+        if len(rs) >= len_rs :
+            break 
+        rs.append(begin)
+        dw_set.append(random.randint(0,1)) 
+        for i in range(add):
+            begin = begin + 1
+            if len(rs) >= len_rs:
+                break
+            rs.append(begin)
+            dw_set.append(random.randint(0,1))
+    elif maxApend==2:
+        if len(rs) >= len_rs:
+            break
+        rs.append(499)
+        dw_set.append(random.randint(0,1))
+        if len(rs) >= len_rs:
+            break
+        rs.append(500)
+        dw_set.append(random.randint(0,1))
+    else:
+        rs.append(500)
+        dw_set.append(random.randint(0,1))
+
+print(len(rs))
+print(len(dw_set))
+
+for z in range(10):
+    fn = input("輸入frame數: ")
+    fn = int(fn)
+    page_fault = 0
+    dw = 0
+    interrupt = 0
+
+    frame = []
+    frame_record = []
+
+    index = 0
+    for i in range(fn):
+        #print(rs[index])
+        frame.append(rs[index])
+        frame_record.append(1)
+        index = index + 1
+        page_fault = page_fault + 1
+
+    #print(len(frame))
+    #print(len(frame_record))
+    print(page_fault)
+    while index < len(rs):
+        #print(frame)
+        #print(">>>",frame_record)
+        #print("")
+        if rs[index] in frame:
+            frame_record[frame.index(rs[index])] = frame_record[frame.index(rs[index])] + 1
+
+            tmp1=frame_record[frame.index(rs[index])]
+            del frame_record[frame.index(rs[index])]
+            frame_record.append(tmp1)
+
+            tmp2 = frame[frame.index(rs[index])]
+            del frame[frame.index(rs[index])]
+            frame.append(tmp2)
+            
+        else:
+            """       
+            min = 100000+1
+            multiple = []
+            for i in range(fn):
+                if frame_record[i] < min :
+                    
+                    min = frame_record[i]
+                elif frame_record[i] == min    
+            if len(multiple) > 1 :
+                frame[multiple]
+            """
+            #multiple = []
+            min_value = min(frame_record)
+            
+            """
+            for i in range(fn):
+                if frame_record[i] == min_value:
+                    multiple.append(i)
+            """
+            """
+            for i in range(frame_record.index(min_value),fn):
+                if i == (fn-1):
+                    frame_record[i] = 1
+                else:
+                    frame_record[i] = frame_record[i+1]
+            """
+
+            #tmp3=frame_record[frame.index(rs[index])]
+
+            for k in range(index-1,0,-1):
+                if rs[k] == frame[frame_record.index(min_value)]:
+                    if dw_set[k] == 1 :
+                        dw = dw + 1
+                    break    
+            
+            frame.remove(frame[frame_record.index(min_value)])     
+            frame.append(rs[index])
+            del frame_record[frame_record.index(min_value)]
+            frame_record.append(1)
+            interrupt = interrupt + 1
+            page_fault = page_fault + 1
+
+        index = index + 1
+    print(page_fault)
+    print(interrupt)
+    print(dw)
+    # 將 (Radom Reference String + Radom dw_set) + Page Fault + Interrupt + disk write 寫入 excel
+    with open('LRU_pro_lrs.csv','a',newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([fn,page_fault,interrupt,dw])
+#print(frame)
+#print(frame_record)
+#print(page_fault)
+        
---
+++ Demo/fifo_custom.py
@@ -0,0 +1,165 @@
+# Page Replacement Algorithms and Evaluation
+## FIFO algorithm - algorithm(1)
+
+# create queue
+import queue
+
+
+
+# Page 編號 : 1~500 
+# Memory Reference 次數 : 至少 10,000 次
+# physical memory 的 frame 數量 : 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 
+## Random : Arbitrarily pick one number for each reference. - Reference String(1)
+
+""" 
+myqueue_10 = queue.Queue(10)  
+myqueue_20 = queue.Queue(20)
+myqueue_30 = queue.Queue(30)
+myqueue_40 = queue.Queue(40)
+myqueue_50 = queue.Queue(50)
+myqueue_60 = queue.Queue(60)
+myqueue_70 = queue.Queue(70)
+myqueue_80 = queue.Queue(80)
+myqueue_90 = queue.Queue(90)
+myqueue_100 = queue.Queue(100)
+"""
+
+# Radom 產生 Reference String 
+## 測試 小範圍 - Textbook FIFO example 1
+"""
+myqueue_3 = queue.Queue(3)
+rs_ex1 = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1]
+page_fault_sum = 0
+cnt = 0 
+while myqueue_3.full()!=True :
+    myqueue_3.put(rs_ex1[cnt])
+    cnt = cnt + 1
+    page_fault_sum = page_fault_sum + 1
+
+print('page_fault_sum:',page_fault_sum)
+
+
+# 塞滿了，要塞下一個 先看 page 是否在 queue 裡 
+# 有 則 hit , 沒有 則 miss 
+
+while cnt != len(rs_ex1) : 
+    if rs_ex1[cnt] in myqueue_3.queue:    
+        pass
+    else:
+        page_fault_sum = page_fault_sum + 1
+        myqueue_3.get()
+        myqueue_3.put(rs_ex1[cnt])
+    cnt = cnt + 1
+print('page_fault_sum:',page_fault_sum)
+"""
+
+## 測試 小範圍 - Textbook FIFO example 2
+"""
+myqueue_3 = queue.Queue(3)
+rs_ex2 = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5]
+page_fault_sum = 0
+cnt = 0 
+while myqueue_3.full()!=True :
+    myqueue_3.put(rs_ex2[cnt])
+    cnt = cnt + 1
+    page_fault_sum = page_fault_sum + 1
+
+print('page_fault_sum:',page_fault_sum)
+
+
+# 塞滿了，要塞下一個 先看 page 是否在 queue 裡 
+# 有 則 hit , 沒有 則 miss 
+
+while cnt != len(rs_ex2) : 
+    if rs_ex2[cnt] in myqueue_3.queue:    
+        pass
+    else:
+        page_fault_sum = page_fault_sum + 1
+        myqueue_3.get()
+        myqueue_3.put(rs_ex2[cnt])
+    cnt = cnt + 1
+print('page_fault_sum:',page_fault_sum)
+"""
+"""
+myqueue_4 = queue.Queue(4)
+rs_ex2 = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5]
+"""
+import csv
+with open('fifo_custom.csv','a',newline='\n') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["numberof_Frame","Page Fault ","Interrupt","disk write"])
+
+
+import random
+
+random_rs = []
+len_rs = 100000
+
+#len_rs = 100000
+# randint(1,500) 1~500
+#random_rs = []
+dw_set = [] # disk write set
+for i in range(len_rs):
+    #random_rs.append(random.randint(1,500))
+    dw_set.append(random.randint(0,1))
+
+
+# Assume 有 50 個 frame # Why 50 ? 因為 10~100 取中 
+
+
+sum = 0
+start = 50
+
+while sum < len_rs:
+    for i in range(1,50):
+        random_rs.append(i)
+        sum = sum + 1
+    random_rs.append(start)
+    start = start + 1
+    sum = sum + 1    
+
+
+
+
+for i in range(10):
+    frame_number = input("Number of frame: ")     
+    myqueue = queue.Queue(int(frame_number))
+    page_fault_sum = 0
+    interrupt = 0
+    disk_write = 0
+
+
+    cnt = 0 
+    while myqueue.full()!=True :
+        myqueue.put(random_rs[cnt])
+        cnt = cnt + 1
+        page_fault_sum = page_fault_sum + 1
+
+    print('page_fault_sum:',page_fault_sum)
+
+
+
+    # 塞滿了，要塞下一個 先看 page 是否在 queue 裡 
+    # 有 則 hit , 沒有 則 miss 
+
+    while cnt != len(random_rs) : 
+        if random_rs[cnt] in myqueue.queue:    
+            pass
+        else:
+            interrupt = interrupt + 1
+            page_fault_sum = page_fault_sum + 1
+            if dw_set[random_rs.index(myqueue.get())] == 1 : # modify
+                disk_write = disk_write + 1
+            #myqueue_10.get()
+            myqueue.put(random_rs[cnt])
+        cnt = cnt + 1
+
+    print('page_fault_sum: ',page_fault_sum)
+    print('inteerrupt: ',interrupt)
+    print('disk write: ',disk_write)
+
+    # 將 (Radom Reference String + Radom dw_set) + Page Fault + Interrupt + disk write 寫入 excel
+    with open('fifo_custom.csv','a',newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([frame_number,page_fault_sum,interrupt,disk_write])
+     
